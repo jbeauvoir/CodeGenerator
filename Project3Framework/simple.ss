b0f47bd77725440b58100f8bded4307fb5819eb3
jbeauvoir <jbeauvoir2011@gmail.com>
2018-05-17 13:28:30 -0700

    Rules 2,8: indenting is not correct because semicolon is not implemented yet

---
+++ Project3Framework/simple.ss
@@ -0,0 +1,1 @@
+(define (listop_ex1) a)
--- Project3Framework/test.cpp
+++ Project3Framework/test.cpp
@@ -4,13 +4,3 @@
 #include "Object.h"
 using namespace std;
 
-int main ()
-{
-	cout << Object(12) + Object(5);
-	cout << endl;
-	cout << Object("abc") + Object("xyz");
-	cout << endl;
-	cout << Object("abc") + Object(5);
-	cout << endl;
-	return 0;
-}
